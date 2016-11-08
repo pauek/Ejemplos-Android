6e0545e0bdd2a7f0614b34c7d272b5fb2dad0d10
Pau Fernández <pau.duran@gmail.com>
2016-11-08 12:06:51 +0100

    Añado proyecto nuevo sobre Notas.

---
+++ Notas/app/src/main/java/info/pauek/notas/ListaNotasActivity.java
@@ -0,0 +1,13 @@
+package info.pauek.notas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaNotasActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_notas);
+    }
+}
