f8c60db990a0ca5c7abe9ba12f5ac00e985c4d46
Pau Fernández <pau.duran@gmail.com>
2016-11-08 13:15:09 +0100

    Mostramos elementos de la lista con layout propio (incluyendo el Adaptador).

--- Notas/app/src/main/java/info/pauek/notas/ListaNotasActivity.java
+++ Notas/app/src/main/java/info/pauek/notas/ListaNotasActivity.java
@@ -1,14 +1,21 @@
 package info.pauek.notas;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class ListaNotasActivity extends AppCompatActivity {
 
+    private NotasAdapter adapter;
     private ArrayList<Nota> notas;
     private ListView lista_notas;
 
@@ -21,11 +28,31 @@ public class ListaNotasActivity extends AppCompatActivity {
         notas.add(new Nota("hola", "que tal"));
         notas.add(new Nota("1234", "567890"));
 
+        adapter = new NotasAdapter();
+
         lista_notas = (ListView) findViewById(R.id.lista_notas);
-        lista_notas.setAdapter(new ArrayAdapter<>(
-                this,
-                android.R.layout.simple_list_item_1,
-                notas
-        ));
+        lista_notas.setAdapter(adapter);
+    }
+
+    private class NotasAdapter extends ArrayAdapter<Nota> {
+        NotasAdapter() {
+            super(ListaNotasActivity.this, R.layout.item_lista_notas, notas);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View result = convertView;
+            if (result == null) {
+                LayoutInflater inflater = getLayoutInflater();
+                result = inflater.inflate(R.layout.item_lista_notas, parent, false);
+            }
+            Nota nota = getItem(position);
+            TextView titulo = (TextView) result.findViewById(R.id.titulo);
+            titulo.setText(nota.getTitulo());
+            TextView resumen = (TextView) result.findViewById(R.id.resumen);
+            resumen.setText(nota.getTexto());
+            return result;
+        }
     }
 }
